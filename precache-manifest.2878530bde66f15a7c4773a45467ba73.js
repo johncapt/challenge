8cb91dc923cd82687853bc0980cd6d23531e7f43
jfccapt <jfc.capt@gmail.com>
2020-08-22 01:37:43 +0800

    Updates

---
+++ precache-manifest.2878530bde66f15a7c4773a45467ba73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "013e384e976efab27d4d8aec3d75a942",
+    "url": "/challenge/index.html"
+  },
+  {
+    "revision": "b8ac941d3568b6d5b244",
+    "url": "/challenge/static/css/main.2efac67c.chunk.css"
+  },
+  {
+    "revision": "38baafbaa8e21ac071ef",
+    "url": "/challenge/static/js/2.177358bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/challenge/static/js/2.177358bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8ac941d3568b6d5b244",
+    "url": "/challenge/static/js/main.7a13f473.chunk.js"
+  },
+  {
+    "revision": "4b96fbf4f96771277489",
+    "url": "/challenge/static/js/runtime-main.0532f63a.js"
+  }
+]);
