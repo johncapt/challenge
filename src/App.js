7f9df94a886b7d5e580ab722c864061daba7317a
jfccapt <jfc.capt@gmail.com>
2020-08-22 01:28:24 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputText: "",
+      inputSubText: "",
+      displayMatches: false,
+    };
+  }
+
+  onButtonClick = (event) => {
+    this.setState({ displayMatches: true });
+  };
+
+  onChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { inputText, inputSubText, displayMatches } = this.state;
+    const indexes = [...inputText.matchAll(new RegExp(inputSubText, "gi"))].map(
+      (a) => a.index
+    );
+
+    return (
+      <div>
+        <h1>Find matches of subtext within text</h1>
+        <div className="App">
+          <div>
+            <label htmlFor="inputText">Text</label>
+            <input
+              name="inputText"
+              value={inputText}
+              onChange={this.onChange}
+              type="text"
+              placeholder="Enter text"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="inputSubText">Sub Text</label>
+            <input
+              name="inputSubText"
+              value={inputSubText}
+              onChange={this.onChange}
+              type="text"
+              placeholder="Enter subtext"
+              required
+            />
+          </div>
+          <button onClick={this.onButtonClick}>
+            Find matches of subtext within text
+          </button>
+
+          <p className="results">
+            {displayMatches && inputSubText !== ""
+              ? `${
+                  indexes.length
+                } match(es) found at position(s):${indexes.join(", ")}`
+              : ""}{" "}
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
